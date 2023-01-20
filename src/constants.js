3f9e4d1ce5dc9a5d6882a52f183049296e685fdc
Taryn Livingstone <livingstonetaryn@gmail.com>
2023-01-19 16:58:25 -0800

    initial commit

---
+++ src/constants.js
@@ -0,0 +1,95 @@
+exports.BUBBLES = [
+    {
+        x: "1200",
+        y: "5",
+        content: "Peaky Blinders",
+        color: "green"
+    },
+    {
+        x: "1200",
+        y: "150",
+        content: "Dark",
+        color: "blue"
+      },
+    {
+        x: "1000",
+        y: "150",
+        content: "Dexter",
+        color: "blue"
+    },
+    {
+      x: "16",
+      y: "0",
+      content: "Breaking bad",
+      color: "pink"
+    },
+    {
+      x: "160",
+      y: "10",
+      content: "Game of thrones",
+      color: "green"
+    },
+    {
+      x: "750",
+      y: "70",
+      content: "Sherlock",
+      color: "green"
+    },
+    {
+      x: "400",
+      y: "0",
+      content: "Mr Robot",
+      color: "blue"
+    },
+    {
+      x: "500",
+      y: "35",
+      content: "Hanibal",
+      color: "green"
+    },
+    {
+      x: "620",
+      y: "10",
+      content: "Rick and Morty",
+      color: "yellow"
+    },
+    {
+      x: "0",
+      y: "83",
+      content: "Fringe",
+      color: "pink"
+    },
+    {
+      x: "160",
+      y: "110",
+      content: "Better Call Saul",
+      color: "green"
+    },
+    {
+      x: "245",
+      y: "68",
+      content: "Sex education",
+      color: "blue"
+    },
+    {
+      x: "850",
+      y: "50",
+      content: "Outsider",
+      color: "yellow"
+    },
+    {
+      x: "400",
+      y: "100",
+      content: "Agents of Shield",
+      color: "blue"
+    },
+    {
+      x: "600",
+      y: "80",
+      content: "Chernobyl",
+      color: "pink"
+    }
+  ];
+  
+  exports.SCROLL_SPEED = 0.6;
+  
